392eaf7893d9550a40bac97d13485af7ab775b64
Joe Endris <jendris@my.apsu.edu>
2023-05-29 14:56:07 -0500

    Update heat_tolerance_plots.R

--- heat_tolerance_plots.R
+++ heat_tolerance_plots.R
@@ -37,15 +37,18 @@ outputs_species <- outputs%>%
   group_by(id)%>%
   dplyr::summarise(across(Tcrit.lci:T95.uci, list(mean=~mean(.))))
 
-outputs_species <- melt(outputs_species, id.vars = 'id', variable.name = 'Threshold')
+str(outputs_species)
 
+as.factor(outputs_species$id)
 
-
-
-full_plot <- ggplot(outputs_species, aes(x = Threshold, y=id)) +
+full_plot <- ggplot(outputs_species, aes(x= outputs_species$Tcrit.mn_mean, y= reorder(id, Tcrit.mn_mean, decreasing = TRUE, color = "red"))) +
   geom_point()+
+  geom_errorbar(data=subset(outputs_species, group_by("id")), aes(ymax=Tcrit.mn_mean+Tcrit.uci_mean,ymin=Tcrit.mn_mean-Tcrit.lci_mean, color = "red"))+
+ # geom_point(x= outputs_species$T50.mn_mean, color = "blue")+
+ # geom_point(x= outputs_species$T95.mn_mean, color = "black")+
   ylab("Species")+
   xlab("Critical Temperature")+
+  xlim(40, 65)+
   theme_bw()+
   theme(legend.position="none")+
   theme(panel.border = element_blank(),  
